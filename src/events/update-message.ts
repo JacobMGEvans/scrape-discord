7a6fecc3a4799f6d3ac4ffee61992506c6f88dc5
Jacob MG Evans <jacobmgevans@gmail.com>
2023-11-28 09:46:30 -0600

    Update Message

--- src/events/update-message.ts
+++ src/events/update-message.ts
@@ -2,18 +2,29 @@ import { ChannelType, Client } from "discord.js";
 import { env, prisma } from "../helpers.ts";
 
 export async function handleMessageCreated(client: Client) {
-  client.on("messageCreate", async (message) => {
+  client.on("messageUpdate", async (message) => {
     if (
       //@ts-expect-error - This is a GuildForumChannel
       message.channel.parent.type !== ChannelType.GuildForum ||
       //@ts-expect-error - This is a GuildForumChannel
       message.channel.parent.id !== env.FORUM
     )
-      return console.error("Message not in Support forum");
+      return console.error("Updated Message not in Support forum");
+
+    if (message.partial)
+      return console.error("Message is partial - Likely User Deleted Message");
 
     const imageData = message.attachments.map((attachment, index) => ({
       id: `${message.id}-image-#${index}`,
       url: attachment.url,
+      messageId: message.id,
+    }));
+
+    const emojiData = message.reactions.cache.map((reaction) => ({
+      id: `${reaction.emoji.name}-${message.id}`,
+      name: reaction.emoji.name,
+      animated: reaction.emoji.animated ?? false,
+      identifier: reaction.emoji.identifier,
       messageId: message.id,
     }));
 
@@ -29,6 +40,10 @@ export async function handleMessageCreated(client: Client) {
           create: imageData,
         },
       },
+    });
+    await prisma.emoji.updateMany({
+      where: { messageId: message.id },
+      data: emojiData,
     });
 
     console.log("MESSAGE CREATED:", JSON.stringify(processedMessage, null, 2));
